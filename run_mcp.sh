cf5ec3f91fe43dfe2912ca268c44b829249c145e
Bob Clough <bob@clough.me>
2017-08-12 13:13:05 +0100

    Added automatic migration on run

--- run_mcp.sh
+++ run_mcp.sh
@@ -6,5 +6,6 @@ while ! pg_isready -h db -p 5432 > /dev/null 2> /dev/null; do
   sleep 1
 done
 
+python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
